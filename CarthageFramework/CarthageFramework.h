3abec40b7480a7cd8128375d4702bd286538923a
Andrey Arzhannikov <andreya@handsome.is>
2017-01-11 18:11:23 +0600

    Create project

---
+++ CarthageFramework/CarthageFramework.h
@@ -0,0 +1,19 @@
+//
+//  CarthageFramework.h
+//  CarthageFramework
+//
+//  Created by Andrey Arzhannikov on 11.01.17.
+//  Copyright © 2017 Andrey Arzhannikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageFramework.
+FOUNDATION_EXPORT double CarthageFrameworkVersionNumber;
+
+//! Project version string for CarthageFramework.
+FOUNDATION_EXPORT const unsigned char CarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageFramework/PublicHeader.h>
+
+
---
+++ CarthageFramework/File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  CarthageFramework
+//
+//  Created by Andrey Arzhannikov on 11.01.17.
+//  Copyright © 2017 Andrey Arzhannikov. All rights reserved.
+//
+
+func greeting() -> String {
+    return "Hellow from Carthage Framework"
+}
